a9c470b75430ba7fc91122559e8dab394bc6fd90
OpenVoxProjectBot <215568489+OpenVoxProjectBot@users.noreply.github.com>
2025-12-31 21:57:06 +0000

    Version 5.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.openvoxproject/jruby-utils "5.2.1-SNAPSHOT"
+(defproject org.openvoxproject/jruby-utils "5.2.1"
   :description "A library for working with JRuby"
   :url "https://github.com/openvoxproject/jruby-utils"
   :license {:name "Apache License, Version 2.0"
